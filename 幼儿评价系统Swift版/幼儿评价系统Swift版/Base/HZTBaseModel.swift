13060aeabd02680451c316cebe604efddfa042da
wxw <962530118@qq.com>
2018-11-29 16:37:37 +0800

    [add]:1.初始化工程  2.添加部分文件

---
+++ 幼儿评价系统Swift版/幼儿评价系统Swift版/Base/HZTBaseModel.swift
@@ -0,0 +1,13 @@
+//
+//  HZTBaseModel.swift
+//  幼儿评价系统Swift版
+//
+//  Created by 王新伟 on 2018/11/29.
+//  Copyright © 2018年 wxw. All rights reserved.
+//
+
+import UIKit
+
+class HZTBaseModel: NSObject {
+
+}
---
+++ 幼儿评价系统Swift版/幼儿评价系统Swift版/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  幼儿评价系统Swift版
+//
+//  Created by 王新伟 on 2018/11/29.
+//  Copyright © 2018年 王新伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
